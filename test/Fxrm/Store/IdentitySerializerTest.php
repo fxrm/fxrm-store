850ecd1993510cd8f74e8cf4d2ddc6e23ef0fc6e
Nick Matantsev <nick@sigmaxis.com>
2013-08-12 04:50:25 +0000

    Unit tests

---
+++ test/Fxrm/Store/IdentitySerializerTest.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Fxrm\Store;
+
+class IdentitySerializerTest extends \PHPUnit_Framework_TestCase {
+    public function setUp() {
+        // backend reference is not used in most calls
+        $this->s = new IdentitySerializer('Fxrm\\Store\\TESTIDENTITY', null);
+    }
+
+    public function testExternNull() {
+        $this->assertSame(null, $this->s->extern(null));
+    }
+
+    public function testExternClassMismatch() {
+        $this->setExpectedException('Exception');
+        $this->s->extern((object)array());
+    }
+
+    public function testExternNewEntity() {
+        // use a test instance with mocked backend
+        $backend = $this->getMock('backend', array('create'));
+        $this->s = new IdentitySerializer('Fxrm\\Store\\TESTIDENTITY', $backend);
+
+        $id = new TESTIDENTITY();
+
+        $backend->expects($this->once())->method('create')->with($this->equalTo('Fxrm\\Store\\TESTIDENTITY'))->will($this->returnValue('TESTID'));
+        $this->assertSame('TESTID', $this->s->extern($id));
+        $this->assertSame('TESTID', $this->s->extern($id)); // run a second time to check
+    }
+
+    public function testIntern() {
+        $id = $this->s->intern('TESTEXISTINGID');
+        $this->assertInstanceOf('Fxrm\\Store\\TESTIDENTITY', $id);
+
+        // must return same reference
+        $this->assertSame($id, $this->s->intern('TESTEXISTINGID'));
+
+        // must convert back to same string
+        $this->assertSame('TESTEXISTINGID', $this->s->extern($id));
+    }
+}
+
+class TESTIDENTITY {
+}
